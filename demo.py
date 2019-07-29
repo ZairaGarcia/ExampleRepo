366af8ac4f2ee7fd9182cea62e193a47f9a41215
Zaira Garcia <zairagarcia@usc-guestwireless-upc-newsc1246.usc.edu>
2019-07-29 10:32:31 -0700

    Added demo.py

---
+++ demo.py
@@ -0,0 +1,15 @@
+#-----------------------------------------------------
+#       Name: Zaira Garcia
+#       Date: 29 July 2019
+#
+# Description: This is a sample python file,
+#              including a header and a main function
+#-----------------------------------------------------
+
+def main():
+
+    name = "Zaira"
+    print("Hello, my name is", name)
+
+if __name__== "__main__":
+    main()
